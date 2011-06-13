3f370ed1f871f0dd3461ae6e12ecac536de56b4c
chromatic <chromatic@wgz.org>
2011-06-12 20:12:43 -0700

    Ported to Dist::Zilla.
    Removed signature test.

--- lib/Pod/ToDemo.pm
+++ lib/Pod/ToDemo.pm
@@ -1,9 +1,8 @@
 package Pod::ToDemo;
+# ABSTRACT: writes a demo program from a tutorial POD
 
 use strict;
-
-use vars '$VERSION';
-$VERSION = '1.01';
+use warnings;
 
 sub import
 {
@@ -147,6 +146,6 @@ No known bugs, now.  Thanks to Greg Lapore for helping me track down a bug in
 
 =head1 COPYRIGHT
 
-Copyright (c) 2003 - 2005, chromatic.  All rights reserved.  You may use,
-modify, and distribute this module under the same terms as Perl 5.8.x, in the
+Copyright (c) 2003 - 2011, chromatic. Some rights reserved. You may use,
+modify, and distribute this module under the same terms as Perl 5.12, in the
 hope that it is useful but certainly under no guarantee.
